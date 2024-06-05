1c162a3e64f1842c14f9a042a56d3bd0f5794953
Aakarsh Sinha <aakarshranjan9@gmail.com>
2024-06-06 00:18:58 +0530

    First Commit

---
+++ src/main/java/com/central/in_books/InBooksApplication.java
@@ -0,0 +1,13 @@
+package com.central.in_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InBooksApplication.class, args);
+	}
+
+}
